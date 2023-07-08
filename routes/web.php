c57c0fe5a0c573a1733425e80ee2e8e8e7717a7c
Renan Aragao <renan.aragao159@gmail.com>
2023-07-08 11:50:10 -0300

    Relação many to many

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::put('/events/update/{id}', [EventController::class, 'update'])->middlewar
 
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
